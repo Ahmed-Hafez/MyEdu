58122fe98acce2701c2ac3fd545d1670500e53f9
Ahmed Hafez <a.hafez@tildetech.ae>
2024-12-14 22:36:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyEdu',
+  webDir: 'www'
+};
+
+export default config;
